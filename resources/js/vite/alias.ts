76f4eb72b6bc83d3efcff8c9668c8a31621eb035
Joshua Anderton <info@joshanderton.com>
2022-11-18 11:34:43 -0800

    Support theming

--- resources/js/vite/alias.ts
+++ resources/js/vite/alias.ts
@@ -1,26 +1,33 @@
-import { searchForWorkspaceRoot } from 'vite'
-import { _env, _set, _merge } from './utils'
+import { UserConfig, searchForWorkspaceRoot } from 'vite'
 import path from 'path'
 
-export default (config: {preserveSymlinks?: boolean} = {}, packagePath: string) => {
+export default (config: UserConfig, packagePath: string) => {
 	
   // Support symlinks for aliasing vendor packages
 	// if (config.preserveSymlinks !== false) {
 	// 	config.preserveSymlinks = true
 	// }
 
-  // Allow aliasing this package
-  config = _merge(config, 'server.fs.allow', [
-    path.resolve(packagePath),
-    searchForWorkspaceRoot(process.cwd())
-  ])
+  const basePath = searchForWorkspaceRoot(process.cwd())
 
-  config = _merge(config, 'resolve.alias', {
+  // Allow aliasing this package
+  config.server = config.server || {}
+  config.server.fs = config.server.fs || {}
+
+  config.server.fs.allow = [
+    ...(config.server.fs.allow || []),
+    path.relative(basePath, packagePath),
+  ]
+
+  config.resolve = config.resolve || {}
+
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
     '@ja-inertia': path.resolve(`${packagePath}/resources/js`),
     '@vendor':     path.resolve('./vendor'),
     '@pckg':       path.resolve('./node_modules'),
     '@':           path.resolve('./resources/js'),
-  })
+  }
 
   return config;
 }
